faa20f0df67ec9623388d4eaee672e199d5e850d
capnerss <capners29@gmail.com>
2026-06-01 18:34:01 +0300

    Add search method to StudentRepository

--- backend/src/main/java/ee/tthk/backend/repository/StudentRepository.java
+++ backend/src/main/java/ee/tthk/backend/repository/StudentRepository.java
@@ -1,11 +1,14 @@
 package ee.tthk.backend.repository;
 
 import ee.tthk.backend.model.Student;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    List<Student> findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName, Sort sort);
 }
